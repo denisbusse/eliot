aaa79b31d41fe14fbb075f2e6557c739a5e10587
Denis Busse <denisbusse@denisbusse.de>
2018-11-29 11:04:06 +0100

    test

--- pkg/runtime/containerd/mapping/containerd.go
+++ pkg/runtime/containerd/mapping/containerd.go
@@ -157,9 +157,9 @@ func mapDevicesToInternalModel(container containers.Container) (result []model.D
 		return result
 	}
 
-	ptrint32 := func(i *int64) uint32 {
-		return uint32(*i)
-	}
+	//ptrint32 := func(i *int64) uint32 {
+	//	return uint32(*i)
+	//}
 
 	for _, device := range spec.Linux.Resources.Devices {
 		result = append(result, model.Device{
